1223b0b3d05e5812a0a46c545e7c9b09a5a66b51
famousyub <famousmeyub@gmail.com>
2023-12-09 09:38:16 +0100

    hjjklmmm

--- cart.php
+++ cart.php
@@ -67,7 +67,7 @@ if(isset($_GET['delete_all'])){
       ?>
       <div class="box">
          <a href="cart.php?delete=<?php echo $fetch_cart['id']; ?>" class="fas fa-times" onclick="return confirm('delete this from cart?');"></a>
-         <img src="uploaded_img/<?php echo $fetch_cart['image']; ?>" alt="">
+         <img src="uploaded_img/<?php echo $fetch_cart['image']; ?>" alt=""  height="220px" width="280px">
          <div class="name"><?php echo $fetch_cart['name']; ?></div>
          <div class="price">$<?php echo $fetch_cart['price']; ?>/-</div>
          <form action="" method="post">
